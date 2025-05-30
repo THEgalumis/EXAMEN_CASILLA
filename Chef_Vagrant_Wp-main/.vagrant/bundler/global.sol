f5ae925114a6db9c6eb9aeccde7714e347cbc304
TELL IVAN CASILLA <telcasillam@upt.pe>
2025-05-30 17:29:52 -0500

    s

--- Chef_Vagrant_Wp-main/.vagrant/bundler/global.sol
+++ Chef_Vagrant_Wp-main/.vagrant/bundler/global.sol
@@ -1,1 +1,1 @@
-{"dependencies":[["dotenv-deployment",["~> 0.0.2"]],["dotenv",["~> 0.9"]],["vagrant-env",["= 0.0.3"]]],"checksum":"3389f64107f5d40b880499ae6da4a525cba3d54b872fb5ed107cf51209cfa691","vagrant_version":"2.4.1"}
+{"dependencies":[["dotenv-deployment",["~> 0.0.2"]],["dotenv",["~> 0.9"]],["vagrant-env",["= 0.0.3"]]],"checksum":"2c6fdb91c949ae56c42e9953ff8a89a157ca5f20f147598f99ae9fda36c5aef2","vagrant_version":"2.4.6"}
